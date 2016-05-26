bc5f74d4c82ffe143974f791ceff1f10b7c88c8a
rix1 <rikardeide@gmail.com>
2016-05-26 13:33:08 +0200

    initial commit on new branch

--- client/node-view/node-view.js
+++ client/node-view/node-view.js
@@ -1,32 +1,34 @@
-import { serialport } from 'serialport';
-import { timesync } from 'timesync';
-import { io } from 'socketio';
+// import { serialport } from 'serialport';
+// import { timesync } from 'timesync';
+// import { io } from 'socketio';
 
-const SerialPort = serialport.SerialPort;
+// const SerialPort = serialport.SerialPort;
 const deviceManufacturer = 'SEGGER';
-
-function getPort(callback){
-	serialport.list(function (err, ports) {
-		ports.forEach(function(port) {
-			if(port.manufacturer === deviceManufacturer){
-				connectToPort(port.comName);
-			}
-		});
-	});
-}
-
-function connectToPort(portName){
-	console.log(`Listening on port ${portName}....`);
-	var port = new SerialPort(portName, {
-		parser: serialport.parsers.readline('\n')
-	});
-	port.on('data', function (data) {
-		console.log('Data: ' + data);
-	});
-}
+const timeSyncServer = 'http://10.24.20.161:8081';
 
 Template.nodeView.onCreated(function () {
 	console.log("template created");
+	//
+	// function getPort(callback){
+	// 	serialport.list(function (err, ports) {
+	// 		ports.forEach(function(port) {
+	// 			// If we find the correct device, connect.
+	// 			if(port.manufacturer === deviceManufacturer){
+	// 				connectToPort(port.comName);
+	// 			}
+	// 		});
+	// 	});
+	// }
+	//
+	// function connectToPort(portName){
+	// 	console.log(`Listening on port ${portName}....`);
+	// 	var port = new SerialPort(portName, {
+	// 		parser: serialport.parsers.readline('\n')
+	// 	});
+	// 	port.on('data', function (data) {
+	// 		console.log('Data: ' + data);
+	// 	});
+	// }
 });
 
 
@@ -34,6 +36,7 @@ Template.nodeView.onRendered(function () {
 	console.log("template rendered");
 
 	var socket1 = io('http://10.24.20.161:8081');
+
 	var ts = timesync.create({
 		server: socket1,
 		interval: 5000
@@ -43,6 +46,4 @@ Template.nodeView.onRendered(function () {
 		var now = new Date(ts.now());
 		console.log(ts.now());
 	}, 1000);
-
-
 });
